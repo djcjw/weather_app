38df7b6e135d0352febeb9852ed550a569c54907
Chris Ward <>
2021-05-11 21:51:47 +0000

    code works for zip and city. missing loops to retry

--- README.md
+++ README.md
@@ -1,19 +1,40 @@
 # weather_app
 This is a useful weather-checking application
+
 My API key:
-
 a346e35af7ce4bdcdff5f710fe121aff
-
-Instructions below:
-
-For your class project we will be creating an application to interact with a webservice in order to obtain data. Your program will use all of the information you’ve learned in the class in order to create a useful application.
-
-Your program must prompt the user for their city or zip code and request weather forecast data from openweathermap.org. Your program must display the weather information in an READABLE format to the user.
 
 Requirements:
 
-Create a Python Application which asks the user for their zip code or city. Use the zip code or city name in order to obtain weather forecast data from: http://openweathermap.org/ Display the weather forecast in a readable format to the user. Use comments within the application where appropriate in order to document what the program is doing. Use functions including a main function. Allow the user to run the program multiple times. Validate whether the user entered valid data. If valid data isn’t presented notify the user. Use the Requests library in order to request data from the webservice. Use Python 3. Use try blocks when establishing connections to the webservice. You must print a message to the user indicating whether or not the connection was successful. Deliverables (More detail on these deliverables is provided in the the weeks they are due):
+Create a Python Application which asks the user for their zip code or city. Use the zip code or city name in order to obtain weather forecast data from: http://openweathermap.org/ 
+
+1. Display the weather forecast in a readable format to the user. 
+
+2. Use comments within the application where appropriate in order to document what the program is doing. 
+
+3. Use functions including a main function. 
+
+4. Allow the user to run the program multiple times.
+
+5. Validate whether the user entered valid data. If valid data isn’t presented notify the user. 
+
+6. Use the Requests library in order to request data from the webservice. Use Python 3. 
+
+7. Use try blocks when establishing connections to the webservice. You must print a message to the user indicating whether or not the connection was successful.
 
 
+#http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_key}
 
-Sign up for API Key http://openweathermap.org/appid The API key will look something similar to this: d5751b1a9e2e4b2b8c7983646072da8b Make a connection to the API using the Requests library.
+#api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_Key}
+
+#Hourly forecast by OpenWeatherMap! Hourly forecast for 4 days, with 96 timestamps and higher geographic accuracy.
+
+#pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid={API key}
+
+#pro.openweathermap.org/data/2.5/forecast/hourly?q={city name},{state code}&appid={API key}
+
+##pro.openweathermap.org/data/2.5/forecast/hourly?q={city name},{state code},{country code}&appid={API key}
+
+#pro.openweathermap.org/data/2.5/forecast/hourly?id={city ID}&appid={API key}
+
+#pro.openweathermap.org/data/2.5/forecast/hourly?id=524901&appid={API_Key}
--- main.py
+++ main.py
@@ -1,137 +1,191 @@
-# My Python program to find current
-# weather details of any city
-# using openweathermap api
 
-#friendly greeting
+  # My Python weather program to find current
+  # weather details of any city
+  # using openweathermap api
+
+  #friendly greeting
 print("Welcome to my super useful weather-checking app.")
 
-#required stuff
-#inporting requuest module
+  #required stuff
+  #inporting requuest module
 import requests
 # base_url variable to store url
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 # My API key
 api_key = "a346e35af7ce4bdcdff5f710fe121aff"
 
+  #user input variables & functions
+  #set the name input variable
+#name = input("\nPlease enter in your name: ")
+#def greet_user():
+    #Function to display a simple titled greeting using inputted name.
+    #print(f"\nHello {name.title()}!")
+    #greet_user()
 
-#user input variables & functions
-#set the name input variable
-name = input("\nPlease enter in your name: ")
-def greet_user():
-  """Function to display a simple titled greeting using inputted name."""
-  print(f"\nHello {name.title()}!")
-greet_user()
-
-
-user_search = input("Enter in a City Name or 5-digit Zip Code: ")
+#setting up loop 
+#def main():
+user_search = input("To search by Zip Code, enter 'z'.\nTo search by City name enter 'c':\n ")
 
 #determine if user_input is a City or a Zip
 
-if user_search == int:
-  print (f"You entered a valid zip. Checking now: ")
-  user_search = zip
-else:
-  city_name = input()
+if user_search == "z":
+  zip_code = input("Enter Zip Code: ")
 
-#def user_info_request():
-  #print("\nPlease enter your 5-digit zip code or city:")
+# complete url address for zip lookup
+  complete_url_zip = base_url + "zip=" + str(zip_code) + "&appid=" + api_key + "&units=imperial"
+  response_c = requests.get(complete_url_zip)
 
-#zip = input('\nPlease enter your zip code:')
-#zip = int(zip)
-#if len(str(zip)) < 5 or len(str(zip)) > 5:
-  #print ("try again")
-  
+  # get method of requests module
+  # return response object
+
+  # json method of response object
+  # convert json format data into
+  # python format data
+  x = response_c.json()
+
+  # Now x contains list of nested dictionaries
+  # Check the value of "cod" key is equal to
+  # "404", means zip code is found otherwise,
+  # zip code is not found
+  if x["cod"] != "404":
+
+    # store the value of "main"
+    # key in variable y
+    y = x["main"]
+
+    # store the value corresponding
+    # to the "temp" key of y
+    current_temperature = y["temp"]
+
+    # store the value corresponding
+    # to the "pressure" key of y
+    current_pressure = y["pressure"]
+
+    # store the value corresponding
+    # to the "humidity" key of y
+    current_humidiy = y["humidity"]
+
+    # store the value corresponding
+    # to the "temp_min" key of y
+    low_temp = y["temp_min"]
+
+    # store the value corresponding
+    # to the "temp_max" key of y
+    high_temp = y["temp_max"]
+
+    # store the value corresponding
+    # to the "feels_like" key of y
+    feels_like = y["feels_like"]
+
+    # store the value of "weather"
+    # key in variable z
+    z = x["weather"]
+
+    # store the value corresponding
+    # to the "description" key at
+    # the 0th index of z
+    weather_description = z[0]["description"]
+
+    # print following values
+    print(" Temperature Forecast (Deg in Fahrenheit) = " +
+          str(current_temperature) +
+        "\n Low temp = " +
+          str(low_temp) +
+        "\n High temp = " +
+          str(high_temp) +
+        "\n Feels like = " +
+          str(feels_like) +
+        "\n Atmospheric Pressure (in hPa unit) = " +
+          str(current_pressure) +
+        "\n Humidity (in percentage) = " +
+          str(current_humidiy) +
+        "\n Description = " +
+          str(weather_description))
+  else:
+    print(" Zip Code Not Found ")
+
+elif user_search == "c":
+  city_name = input("Enter US City name: ")
+
+#starting web REST API setup
+# complete url address for city lookup
+  complete_url_city = base_url + "appid=" + api_key + "&q=" + city_name + "&units=imperial"
+  response_c = requests.get(complete_url_city)
+
+  # get method of requests module
+  # return response object
+  # json method of response object
+  # convert json format data into
+  # python format data
+  x = response_c.json()
+
+  # Now x contains list of nested dictionaries
+  # Check the value of "cod" key is equal to
+  # "404", means zip code is found otherwise,
+  # zip code is not found
+  if x["cod"] != "404":
+
+    # store the value of "main"
+    # key in variable y
+    y = x["main"]
+
+    # store the value corresponding
+    # to the "temp" key of y
+    current_temperature = y["temp"]
+
+    # store the value corresponding
+    # to the "pressure" key of y
+    current_pressure = y["pressure"]
+
+    # store the value corresponding
+    # to the "humidity" key of y
+    current_humidiy = y["humidity"]
+
+    # store the value corresponding
+    # to the "temp_min" key of y
+    low_temp = y["temp_min"]
+
+    # store the value corresponding
+    # to the "temp_max" key of y
+    high_temp = y["temp_max"]
+
+    # store the value corresponding
+    # to the "feels_like" key of y
+    feels_like = y["feels_like"]
+
+    # store the value of "weather"
+    # key in variable z
+    z = x["weather"]
+
+    # store the value corresponding
+    # to the "description" key at
+    # the 0th index of z
+    weather_description = z[0]["description"]
+
+    # print following values
+    print(" Temperature Forecast (Deg in Fahrenheit) = " +
+          str(current_temperature) +
+        "\n Low temp = " +
+          str(low_temp) +
+        "\n High temp = " +
+          str(high_temp) +
+        "\n Feels like = " +
+          str(feels_like) +
+        "\n Atmospheric Pressure (in hPa unit) = " +
+          str(current_pressure) +
+        "\n Humidity (in percentage) = " +
+          str(current_humidiy) +
+        "\n Description = " +
+          str(weather_description))
+   
+  else:
+    print(" City Not Found ")
+
+#print ("\nDo you want to try another location?")
+#user_again = input("Enter 'y' or any key to exit: ")
+#if user_again == 'y':
+  #main()
 #else:
-  #user_zip = zip
-  #print (user_zip)
-  #print (f"You entered a valid zip. Checking now: ")
+  #print ("\nThank you for using my program!")
+#main()
 
-    #return zip
-
-#starting web REST API setup 
-
-
-
-
-
-# Give city name
-
-
-# complete_url variable to store
-# complete url address
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name + "&units=imperial"
-
-#call for weather using zip
-#api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
-
-# get method of requests module
-# return response object
-response = requests.get(complete_url)
-
-# json method of response object
-# convert json format data into
-# python format data
-x = response.json()
-
-# Now x contains list of nested dictionaries
-# Check the value of "cod" key is equal to
-# "404", means city is found otherwise,
-# city is not found
-if x["cod"] != "404":
-
-	# store the value of "main"
-	# key in variable y
-	y = x["main"]
-
-	# store the value corresponding
-	# to the "temp" key of y
-	current_temperature = y["temp"]
-
-	# store the value corresponding
-	# to the "pressure" key of y
-	current_pressure = y["pressure"]
-
-	# store the value corresponding
-	# to the "humidity" key of y
-	current_humidiy = y["humidity"]
-
-	# store the value of "weather"
-	# key in variable z
-	z = x["weather"]
-
-	# store the value corresponding
-	# to the "description" key at
-	# the 0th index of z
-	weather_description = z[0]["description"]
-
-	# print following values
-	print(" Temperature (in Fahrenheit) = " +
-					str(current_temperature) +
-		"\n atmospheric pressure (in hPa unit) = " +
-					str(current_pressure) +
-		"\n humidity (in percentage) = " +
-					str(current_humidiy) +
-		"\n description = " +
-					str(weather_description))
-
-else:
-	print(" City Not Found ")
-
-
-
-
-#http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_key}
-
-#api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_Key}
-
-#Hourly forecast by OpenWeatherMap! Hourly forecast for 4 days, with 96 timestamps and higher geographic accuracy.
-
-#pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid={API key}
-
-#pro.openweathermap.org/data/2.5/forecast/hourly?q={city name},{state code}&appid={API key}
-
-##pro.openweathermap.org/data/2.5/forecast/hourly?q={city name},{state code},{country code}&appid={API key}
-
-#pro.openweathermap.org/data/2.5/forecast/hourly?id={city ID}&appid={API key}
-
-#pro.openweathermap.org/data/2.5/forecast/hourly?id=524901&appid={API_Key}
